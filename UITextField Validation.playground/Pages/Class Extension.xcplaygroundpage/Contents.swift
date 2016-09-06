846711be66fec658df46abee4b416466501e81cd
klein-thibault <klein.thibault@gmail.com>
2016-09-06 14:46:10 +0200

    Updated the playground to Swift 3 and added a page with a protocol implementation

--- UITextField Validation.playground/Pages/Class Extension.xcplaygroundpage/Contents.swift
+++ UITextField Validation.playground/Pages/Class Extension.xcplaygroundpage/Contents.swift
@@ -1,48 +1,46 @@
-//: UITextField validation
-
 import UIKit
 
 extension UITextField {
 
-    func validateField(functions: [String -> Bool]) -> Bool {
+    func validateField(_ functions: [(String) -> Bool]) -> Bool {
         return functions.map { f in f(self.text ?? "") }.reduce(true) { $0 && $1 }
     }
-    
+
 }
 
 extension String {
-    func evaluate(regexp: String) -> Bool {
-        guard let range = rangeOfString(regexp, options: .RegularExpressionSearch) else {
+    func evaluate(with condition: String) -> Bool {
+        guard let range = range(of: condition, options: .regularExpression, range: nil, locale: nil) else {
             return false
         }
-        return range.startIndex == startIndex
-            && range.endIndex == endIndex
+
+        return range.lowerBound == startIndex && range.upperBound == endIndex
     }
 }
 
 func isPhoneNumberValid(text: String) -> Bool {
     let regexp = "^[0-9]{10}$"
-    return text.evaluate(regexp)
+    return text.evaluate(with: regexp)
 }
 
 func isZipCodeValid(text: String) -> Bool {
     let regexp = "^[0-9]{5}$"
-    return text.evaluate(regexp)
+    return text.evaluate(with: regexp)
 }
 
 func isStateValid(text: String) -> Bool {
     let regexp = "^[A-Z]{2}$"
-    return text.evaluate(regexp)
+    return text.evaluate(with: regexp)
 }
 
 func isCVCValid(text: String) -> Bool {
     let regexp = "^[0-9]{3,4}$"
-    return text.evaluate(regexp)
+    return text.evaluate(with: regexp)
 }
 
 func isEmailValid(text: String) -> Bool {
     let regexp = "[A-Z0-9a-z._]+@([\\w\\d]+[\\.\\w\\d]*)"
-    return text.evaluate(regexp)
+    return text.evaluate(with: regexp)
 }
 
 let cvcTextField = UITextField()
---
+++ UITextField Validation.playground/Pages/Protocol.xcplaygroundpage/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+protocol Validatable {
+    associatedtype T
+
+    func validate(_ functions: [T]) -> Bool
+}
+
+protocol Evaluatable {
+    associatedtype T
+
+    func evaluate(with condition: T) -> Bool
+}
+
+extension UITextField: Validatable {
+    func validate(_ functions: [(String) -> Bool]) -> Bool {
+        return functions.map { f in f(self.text ?? "") }.reduce(true) { $0 && $1 }
+    }
+}
+
+extension String: Evaluatable {
+    func evaluate(with condition: String) -> Bool {
+        guard let range = range(of: condition, options: .regularExpression, range: nil, locale: nil) else {
+            return false
+        }
+
+        return range.lowerBound == startIndex && range.upperBound == endIndex
+    }
+}
+
+func isPhoneNumberValid(text: String) -> Bool {
+    let regexp = "^[0-9]{10}$"
+    return text.evaluate(with: regexp)
+}
+
+func isZipCodeValid(text: String) -> Bool {
+    let regexp = "^[0-9]{5}$"
+    return text.evaluate(with: regexp)
+}
+
+func isStateValid(text: String) -> Bool {
+    let regexp = "^[A-Z]{2}$"
+    return text.evaluate(with: regexp)
+}
+
+func isCVCValid(text: String) -> Bool {
+    let regexp = "^[0-9]{3,4}$"
+    return text.evaluate(with: regexp)
+}
+
+func isEmailValid(text: String) -> Bool {
+    let regexp = "[A-Z0-9a-z._]+@([\\w\\d]+[\\.\\w\\d]*)"
+    return text.evaluate(with: regexp)
+}
+
+let cvcTextField = UITextField()
+cvcTextField.text = "123"
+cvcTextField.validate([isCVCValid])
+
+struct User {
+    let firstName: String
+    let lastName: String
+    let age: Int
+}
+
+extension User: Validatable {
+    func validate(_ functions: [(User) -> Bool]) -> Bool {
+        return functions.map { f in f(self) }.reduce(true) { $0 && $1 }
+    }
+}
+
+func isUserNameValid(user: User) -> Bool {
+    let regexp = "[A-Za-z]+"
+    return user.firstName.evaluate(with: regexp) && user.lastName.evaluate(with: regexp)
+}
+
+func isUserAdult(user: User) -> Bool {
+    return user.age >= 18
+}
+
+let user = User(firstName: "Thibault", lastName: "Klein", age: 18)
+user.validate([isUserNameValid, isUserAdult])
